9a6a95bf204b2be34ff26dd416d39cd17eb63c44
Haruki Nakano <haruki.nakano@gree.net>
2015-07-01 14:18:10 -0700

    Create GameScene

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,19 @@
+//
+//  GameScene.cpp
+//  Tetrominos
+//
+//  Created by Haruki Nakano on 2015/07/01.
+//
+//
+
+#include "GameScene.h"
+
+using namespace cocos2d;
+
+bool GameScene::init() {
+    if (!Node::init()) {
+        return false;
+    }
+
+    return true;
+}
---
+++ Classes/GameScene.h
@@ -0,0 +1,22 @@
+//
+//  GameScene.h
+//  Tetrominos
+//
+//  Created by Haruki Nakano on 2015/07/01.
+//
+//
+
+#ifndef __Tetrominos__GameScene__
+#define __Tetrominos__GameScene__
+
+#include "cocos2d.h"
+
+class GameScene : public cocos2d::Node {
+public:
+    CREATE_FUNC(GameScene);
+
+private:
+    bool init() override;
+};
+
+#endif /* defined(__Tetrominos__GameScene__) */
